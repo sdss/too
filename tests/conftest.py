13f7fba83cfd2b055c88a0a1653aa4a4bb767976
José Sánchez-Gallego <gallegoj@uw.edu>
2024-02-11 22:01:44 -0800

    Add initial tests for the database

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2024-02-11
+# @Filename: conftest.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+import pytest
+
+
+DBNAME: str = "sdss5db_too_test"
+
+
+@pytest.fixture(autouse=True, scope="session")
+def sdss5db():
+    """A fixture that returns a connection to the ToO test database."""
+
+    from sdssdb.peewee import sdss5db
+
+    sdss5db.database.connect(DBNAME)
+
+    yield
+
+    sdss5db.database.close()
---
+++ tests/test_database.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2024-02-11
+# @Filename: test_database.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+from __future__ import annotations
+
+from conftest import DBNAME
+from sdssdb.peewee.sdss5db import catalogdb
+
+
+def test_database_exists():
+    assert catalogdb.Catalog.table_exists()
+    assert catalogdb.database.dbname == DBNAME
+
+    n_catalog = catalogdb.Catalog.select().count()
+    assert n_catalog > 1000000 and n_catalog < 10000000
+
+
+def test_models_exist():
+    assert catalogdb.SDSS_DR13_PhotoObj.table_exists()
+    assert catalogdb.CatalogToSDSS_DR13_PhotoObj_Primary.table_exists()  # type: ignore
+    assert catalogdb.SDSS_DR13_PhotoObj.select().count() > 1
+    assert catalogdb.CatalogToSDSS_DR13_PhotoObj_Primary.select().count() > 1  # type: ignore
+
+    assert catalogdb.Gaia_DR3.table_exists()
+    assert catalogdb.CatalogToGaia_DR3.table_exists()  # type: ignore
+    assert catalogdb.Gaia_DR3.select().count() > 1
+    assert catalogdb.CatalogToGaia_DR3.select().count() > 1  # type: ignore
+
+    assert catalogdb.TwoMassPSC.table_exists()
+    assert catalogdb.CatalogToTwoMassPSC.table_exists()  # type: ignore
+    assert catalogdb.TwoMassPSC.select().count() > 1
+    assert catalogdb.CatalogToTwoMassPSC.select().count() > 1  # type: ignore
