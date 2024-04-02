e372339eb4b40edbbdefc468d789443d559b5622
imedan <ilija.medan@gmail.com>
2024-04-02 15:21:41 -0500

    add initial bright neighbor validation code

---
+++ src/too/validate.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: Ilija Medan (ilija.medan@vanderbilt.edu)
+# @Date: 2024-04-02
+# @Filename: validate.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+import os
+import polars
+import numpy as np
+import pickle
+from astropy_healpix import HEALPix
+from astropy.coordinates import SkyCoord
+import astropy.units as u
+
+# load enviornment variable with path to healpix maps
+BN_HEALPIX = os.getenv('BN_HEALPIX')
+
+
+def bn_validation(targets: polars.DataFrame, design_mode: str):
+    """
+    Validate a ToO to see if it is too close
+    to a bright neighbor. This functionm relies on the
+    healpix map of bright neighbors for each designmode
+    created with this script:
+    https://github.com/sdss/mugatu/blob/master/bin/bright_neigh_healpix_map.py.
+    To use this function, the enviornment varriable BN_HEALPIX must be
+    set to the path to these healpix maps.
+
+    Parameters
+    ----------
+    targets: polars.DataFrame
+        DataFrame with the ToO target information
+
+    design_mode: str
+        The design_mode to run the validation for
+    """
+    # load the healpix indicies for the designmode
+    bn_maps_boss_file = '{BN_HEALPIX}/{design_mode}_{instrument}_bn_healpix.pkl'\
+                            .format(BN_HEALPIX=BN_HEALPIX,
+                                    instrument='boss',
+                                    design_mode=design_mode)
+    with open(bn_maps_boss_file, 'rb') as f:
+        bn_maps_boss = pickle.load(f)
+
+    bn_maps_apogee_file = '{BN_HEALPIX}/{design_mode}_{instrument}_bn_healpix.pkl'\
+                              .format(BN_HEALPIX=BN_HEALPIX,
+                                      instrument='apogee',
+                                      design_mode=design_mode)
+    with open(bn_maps_apogee_file, 'rb') as f:
+        bn_maps_apogee = pickle.load(f)
+
+    # create the correct nside healpix object
+    hp = HEALPix(nside=2 ** 18, order='ring', frame='icrs')
+
+    # get the indicies for all targets
+    coord = SkyCoord(ra=targets['ra'].to_numpy() * u.deg,
+                     dec=targets['dec'].to_numpy() * u.deg)
+    hp_inds = hp.skycoord_to_healpix(coord)
+
+    # check if ToOs in bright neighbor healpixels
+    valid_too = np.zeros(len(targets), dtype=bool) + True
+
+    # in future, is there faster way to do this? would using sets be better?
+    ev_boss = targets['fiber_type'] == 'BOSS'
+    valid_too[ev_boss] = ~np.isin(hp_inds[ev_boss], bn_maps_boss)  # oppisite as True == valid target
+
+    ev_apogee = targets['fiber_type'] == 'APOGEE'
+    valid_too[ev_apogee] = ~np.isin(hp_inds[ev_apogee], bn_maps_apogee)  # oppisite as True == valid target
+    return valid_too
