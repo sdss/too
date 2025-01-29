1a888d0b8b3966e523cd3458267a789b58d50862
José Sánchez-Gallego <gallegoj@uw.edu>
2025-01-28 19:53:29 -0700

    Fixes to the cronjob script

--- scripts/cronjob.sh
+++ scripts/cronjob.sh
@@ -3,6 +3,10 @@
 # Exit on error
 set -e
 
+module use /uufs/chpc.utah.edu/common/home/sdss50/software/modulefiles
+module load sdsscore/main
+module load fps_calibrations/main
+
 SDSSCORE_APO_TOO=$SDSSCORE_DIR/apo/too_targets
 SDSSCORE_LCO_TOO=$SDSSCORE_DIR/lco/too_targets
 SDSS_ID_DIFFS_PATH=/uufs/chpc.utah.edu/common/home/sdss50/sdsswork/sandbox/sdss_id/too_diffs
@@ -22,13 +26,9 @@ DATE=`date "+%Y-%m-%d"`
 cd /uufs/chpc.utah.edu/common/home/sdssunit/software/github/sdss/too
 source .venv/bin/activate
 
-module use /uufs/chpc.utah.edu/common/home/sdss50/software/modulefiles
-module load sdsscore/main
-module load fps_calibrations/main
-
 too process --ignore-invalid --cross-match --run-carton -v $TOO_TARGET_FILES
 
-too dump-targets "$SDSSCORE_APO_TOO"/too_targets_apo_${DATE}.parquet APO
+too dump-targets $SDSSCORE_APO_TOO/too_targets_apo_$DATE.parquet --observatory APO
 
 mkdir -p $SDSS_ID_DIFFS_PATH/$DATE
 too dump-sdss-id --root $SDSS_ID_DIFFS_PATH/$DATE $DATE
